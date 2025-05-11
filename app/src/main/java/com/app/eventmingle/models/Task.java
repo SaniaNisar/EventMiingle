2e0002d52231e9912820de6bedf4f9a526d7685a
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-05-11 16:32:26 +0500

    task model added

--- app/src/main/java/com/app/eventmingle/models/Task.java
+++ app/src/main/java/com/app/eventmingle/models/Task.java
@@ -1,4 +1,71 @@
 package com.app.eventmingle.models;
 
 public class Task {
+    private String taskId;
+    private String title;
+    private String description;
+    private String assignedTo; // userId of manager
+    private String dueDate;
+    private String status; // pending, completed
+
+    public Task() {}
+
+    public Task(String taskId, String title, String description, String assignedTo,
+                String dueDate, String status) {
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.assignedTo = assignedTo;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
