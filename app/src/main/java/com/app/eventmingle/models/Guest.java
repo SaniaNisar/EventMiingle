a6a1e1cf3b7b0461a37a5827aa5f2fb3b3b6c59f
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-05-11 16:31:15 +0500

    guest model class added

--- app/src/main/java/com/app/eventmingle/models/Guest.java
+++ app/src/main/java/com/app/eventmingle/models/Guest.java
@@ -1,4 +1,39 @@
 package com.app.eventmingle.models;
 
 public class Guest {
+    private String userId;
+    private String status; // invited, accepted, declined
+    private String arrivalTime;
+
+    public Guest() {}
+
+    public Guest(String userId, String status, String arrivalTime) {
+        this.userId = userId;
+        this.status = status;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 }
