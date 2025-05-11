643b66360bbc280c15c08be7e4ab83cfeb07f44f
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-05-12 04:38:30 +0500

    Events fragment updated

--- app/src/main/java/com/app/eventmingle/fragments/EventsFragment.java
+++ app/src/main/java/com/app/eventmingle/fragments/EventsFragment.java
@@ -2,13 +2,30 @@ package com.app.eventmingle.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.app.eventmingle.adapters.EventAdapter;
+import com.app.eventmingle.models.Event;
+
 
 import com.app.eventmingle.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +42,11 @@ public class EventsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private RecyclerView recyclerView;
+    private EventAdapter eventAdapter;
+    private List<Event> eventList = new ArrayList<>();
+    private DatabaseReference eventsRef;
+
 
     public EventsFragment() {
         // Required empty public constructor
@@ -61,6 +83,39 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        //return inflater.inflate(R.layout.fragment_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerViewEvents);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        eventAdapter = new EventAdapter(eventList);
+        recyclerView.setAdapter(eventAdapter);
+
+        eventsRef = FirebaseDatabase.getInstance().getReference("events");
+        loadEvents();
+
+        return view;
+
     }
+
+    private void loadEvents() {
+        eventsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                eventList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Event event = dataSnapshot.getValue(Event.class);
+                    eventList.add(event);
+                }
+                eventAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error (optional)
+            }
+        });
+    }
+
 }
